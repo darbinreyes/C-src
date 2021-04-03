e95f88819f8b392f42f8caf2eb90393f6b4644c7
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-04-03 05:52:18 -0400

    It works. I don't need to sync with upstream, I just want to add my comments and look at the code.

--- Linux/glibc/ctype/ctype.h
+++ Linux/glibc/ctype/ctype.h
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
-
+// YO moma so fat...
 /*
  *	ISO C99 Standard 7.4: Character handling	<ctype.h>
  */
